15bd55a964aeca57621e0764cfde1e25dbe29c66
Tallest29 <akinadeboyef@gmail.com>
2023-06-20 15:02:53 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: "print the alphabet"
+ * Return: Always 0
+ */
+int main(void)
+{
+	int t;
+
+	for (t = 97; t < 122; t++)
+	{
+		_putchar(t);
+	}
+	_putchar('\n');
+	return (0);
+}
