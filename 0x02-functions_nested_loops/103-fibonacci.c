af72ea19b8b3684ec577d4432a4e25759114a60a
Tallest29 <akinadeboyef@gmail.com>
2023-06-20 20:49:41 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+  *main - Fibonacci sequence
+  *less than 4000000
+  *Return: Nothing!
+  */
+int main(void)
+{
+	int i;
+	long j = 1;
+	long k = 2;
+
+	sum = k;
+
+	while (k + j < 4000000)
+	{
+		k += j;
+		if (k % 2 == 0)
+		su. += k;
+		j = k - j;
+		++i;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
